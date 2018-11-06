b8ef177b2639d0a357fe7073e77e723be8b17eae
shaniphankar <shaniphankar@gmail.com>
2018-11-06 20:03:03 +0530

    Nothing much changed

--- base.cpp
+++ base.cpp
@@ -5,23 +5,111 @@
 #include <unordered_map>
 #include <GL/glut.h>
 #include "glRoutines.h"
+using namespace std;
+double fArray[64][64];
+void drawEmptyClass()
+{
+	glBegin(GL_QUADS);
+	glColor3f(0.5f,0.35f,0.05f);//TOP
+	glVertex3f(1.0f,1.0f,-1.0f);
+	glVertex3f(-1.0f,1.0f,-1.0f);
+	glVertex3f(-1.0f,1.0f,0.0f);
+	glVertex3f(1.0f,1.0f,0.0f);
+	glColor3f(0.5f,0.35f,0.05f);//BOTTOM
+	glVertex3f(1.0f,-1.0f,-1.0f);
+	glVertex3f(-1.0f,-1.0f,-1.0f);
+	glVertex3f(-1.0f,-1.0f,0.0f);
+	glVertex3f(1.0f,-1.0f,0.0f);
+	glColor3f(0.5f,0.35f,0.05f);//BACK
+	glVertex3f(1.0f,1.0f,-1.0f);
+	glVertex3f(-1.0f,1.0f,-1.0f);
+	glVertex3f(-1.0f,-1.0f,-1.0f);
+	glVertex3f(1.0f,-1.0f,-1.0f);
+	glColor3f(0.5f,0.35f,0.05f);//LEFT
+	glVertex3f(-1.0f,1.0f,-1.0f);
+	glVertex3f(-1.0f,1.0f,0.0f);
+	glVertex3f(-1.0f,-1.0f,0.0f);
+	glVertex3f(-1.0f,-1.0f,-1.0f);
+	glColor3f(0.5f,0.35f,0.05f);//RIGHT
+	glVertex3f(1.0f,1.0f,0.0f);
+	glVertex3f(1.0f,1.0f,-1.0f);
+	glVertex3f(1.0f,-1.0f,-1.0f);
+	glVertex3f(1.0f,-1.0f,0.0f);
+	glEnd();
+	glBegin(GL_LINES);
+	glColor3f(0.0f,0.0f,0.0f);//BACK TOP
+	glVertex3f(1.0f,1.0f,-1.0f);
+	glVertex3f(-1.0f,1.0f,-1.0f);
+	glColor3f(0.0f,0.0f,0.0f);//BACK BOTTOM
+	glVertex3f(-1.0f,-1.0f,-1.0f);
+	glVertex3f(1.0f,-1.0f,-1.0f);
+	glColor3f(0.0f,0.0f,0.0f);//BACK LEFT
+	glVertex3f(-1.0f,1.0f,-1.0f);
+	glVertex3f(-1.0f,-1.0f,-1.0f);
+	glColor3f(0.0f,0.0f,0.0f);//BACK RIGHT
+	glVertex3f(1.0f,1.0f,-1.0f);
+	glVertex3f(1.0f,-1.0f,-1.0f);
+	glColor3f(0.0f,0.0f,0.0f);//TOP LEFT
+	glVertex3f(-1.0f,1.0f,0.0f); 
+	glVertex3f(-1.0f,1.0f,-1.0f);
+	glColor3f(0.0f,0.0f,0.0f);//BOTTOM LEFT
+	glVertex3f(-1.0f,-1.0f,-1.0f);
+	glVertex3f(-1.0f,-1.0f,0.0f);
+	glColor3f(0.0f,0.0f,0.0f);//TOP RIGHT
+	glVertex3f(1.0f,1.0f,-1.0f);
+	glVertex3f(1.0f,1.0f,0.0f);
+	glColor3f(0.0f,0.0f,0.0f);//BOTTOM RIGHT
+	glVertex3f(1.0f,-1.0f,0.0f);
+	glVertex3f(1.0f,-1.0f,-1.0f);
+	glEnd();
+
+      
+}
+
+double simpleFunc(double u,double v)
+{
+	int uTemp=round(64*u);
+	int vTemp=round(64*v);
+	return fArray[uTemp][vTemp];
+}
 
 void myDisplay(void)
 {
 	glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+	glTranslatef(0.0f,0.0f,-2.5f);
+	drawEmptyClass();
 	glutSwapBuffers();
 }
 
+void reshape(GLsizei width, GLsizei height)
+{
+	if(height==0)
+		height=1;
+	GLfloat aspect=(GLfloat)width/(GLfloat)height;
+	glViewport(0,0,width,height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0f,aspect,0.1f,100.0f);
+}
 int main(int argc, char** argv)
 {
-	glutInit(&argc, argv);
-	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize (1000,1000);
-	glutInitWindowPosition(100,150);
-	glutCreateWindow ("Wrinkles");
-	glutDisplayFunc(myDisplay);	
-	myInit ();
-	glutMainLoop();
+	for(int i=0;i<64;i++)
+	{
+		for(int j=0;j<64;j++)
+		{
+			fArray[i][j]=((double)rand()/(double)(RAND_MAX));
+			cout<<fArray[i][j];
+		}
+	}
+
+	// glutInit(&argc, argv);
+	// glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+	// glutInitWindowSize (1000,1000);
+	// glutInitWindowPosition(100,150);
+	// glutCreateWindow ("Wrinkles");
+	// glutDisplayFunc(myDisplay);	
+	// glutReshapeFunc(reshape);
+	// myInit ();
+	// glutMainLoop();
 }
